596d4bc87549a01eae7b2578deec6d7b190aecc6
Mads Ohm Larsen <mads.ohm@gmail.com>
2013-11-20 22:32:40 +0100

    Solved 14

---
+++ Haskell/014.hs
@@ -0,0 +1,12 @@
+import Data.List
+
+main = print no014
+
+collatz n = collatz' n 1
+  where
+    collatz' 1 b = b
+    collatz' x b
+           | even x == 0 = collatz' (x `div` 2) (b+1)
+           | otherwise   = collatz' (3*x + 1) (b+1)
+
+no014 = maximumBy (\a b -> compare (snd a) (snd b)) $ take 1000000 [(x, collatz x) | x <- [1..]]
